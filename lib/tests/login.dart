195609a6df6daa6b6695406115960cb420787b94
StanleyAbotsikuma <abotsistanley0@gmail.com>
2023-07-20 18:02:07 +0000

    staging 4

--- lib/tests/login.dart
+++ lib/tests/login.dart
@@ -5,6 +5,42 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            Container(
+              width: 324,
+              height: 51,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(45),
+                color: Color(0xff149a57),
+              ),
+              child: MaterialButton(
+                onPressed: () {},
+                child: Text('Button'),
+                textColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 12),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(45),
+                ),
+              ),
+            ),
+            Container(
+              width: 324,
+              height: 51,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(45),
+                color: Color(0xfff2f2f2),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                ),
+              ),
+            ),
+          ],
+        ));
   }
 }
