142df0ac1f505c27b8a0912e50703b7f5a1e02cd
StanleyAbotsikuma <abotsistanley0@gmail.com>
2023-07-21 16:33:56 +0000

    sign up

---
+++ lib/configs/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const green = Color(0xff149a57);
+  static const whine = Color(0xff9b0505);
+  static const ashLight = Color(0xffF2F2F2);
+}
