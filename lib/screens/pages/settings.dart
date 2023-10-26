752d086e2bee3cded762f1f2d9cfbd3504a2bcca
StanleyAbotsikuma <abotsistanley0@gmail.com>
2023-10-26 01:14:41 +0000

    check point bc db check point

--- lib/screens/pages/settings.dart
+++ lib/screens/pages/settings.dart
@@ -18,32 +18,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int alertTime = 3;
-  bool _buttonPressed = false;
-  bool _countDownActive = false;
-  void _increaseCounterWhilePressed() async {
-    if (_countDownActive) return;
-    _countDownActive = true;
-    while (_buttonPressed) {
-      alertTime--;
-      setState(() {});
-      await Future.delayed(const Duration(milliseconds: 1000));
-      if (alertTime == 0) {
-        alertTime = 3;
-        _buttonPressed = false;
-        // ignore: use_build_context_synchronously
-        Navigator.of(context)
-            .push(MaterialPageRoute(
-          builder: (context) => const CallScreen(),
-        ))
-            .then((value) {
-          setState(() {});
-        });
-      }
-    }
-    _countDownActive = false;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -55,52 +29,7 @@ class _HomePageState extends State<HomePage> {
             child: title("ARE YOU IN AN \n EMERGENCY?"),
           ),
           Gap(20.h),
-          Text("Press and hold for $alertTime seconds",
-              style: GoogleFonts.jura(
-                  textStyle: TextStyle(
-                color: const Color(0xffB64949),
-                letterSpacing: .5,
-                fontWeight: FontWeight.bold,
-                fontSize: 14.sp,
-              ))),
           Gap(28.h),
-          Container(
-            width: 155.h,
-            height: 155.w,
-            padding: EdgeInsets.all(15.h),
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: const Color(0xffb64949),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.withOpacity(0.5),
-                    spreadRadius: 2,
-                    blurRadius: 5,
-                    offset: const Offset(0, 3),
-                  )
-                ]),
-            child: Listener(
-              onPointerDown: (details) {
-                _buttonPressed = true;
-                _increaseCounterWhilePressed();
-              },
-              onPointerUp: (details) {
-                _buttonPressed = false;
-                alertTime = 3;
-                setState(() {});
-              },
-              child: MaterialButton(
-                onPressed: () {},
-                color: const Color(0xffa81717),
-                shape: const CircleBorder(),
-                child: SvgPicture.asset(
-                  AppAssets.alertIcon,
-                  width: 69.33.w,
-                  height: 52.h,
-                ),
-              ),
-            ),
-          ),
           Expanded(
             child: Container(
               margin: EdgeInsets.only(top: 25.h, right: 10.w, left: 10.w),
@@ -109,7 +38,7 @@ class _HomePageState extends State<HomePage> {
                   borderRadius: BorderRadius.circular(25)),
               child: ListView.builder(
                   padding: EdgeInsets.zero,
-                  itemCount: 1,
+                  itemCount: 6,
                   itemBuilder: (BuildContext context, int index) {
                     return Container(
                         width: 348.w,
